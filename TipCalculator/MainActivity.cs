21c9fe9bca44640c3d198f953a313f64a2340898
Wilman Idbyron Garcia De Leon <wdleon@sistran.com.gt>
2018-09-11 11:20:10 -0600

    First commit for TipCalculator

---
+++ TipCalculator/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Widget;
+using System;
+
+namespace TipCalculator
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        EditText inputBill;
+        Button calculateButton;
+        TextView outputTip;
+        TextView outputTotal;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.activity_main);
+            inputBill = FindViewById<EditText>(Resource.Id.inputBill);
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+            calculateButton.Click += OnCalculateClick;
+        }
+
+        void OnCalculateClick(object sender, EventArgs e) {
+            string text = inputBill.Text;
+            var bill = double.Parse(text);
+            var tip = bill * 0.15;
+            var total = bill * 1.15;
+
+            outputTip.Text = tip.ToString();
+            outputTotal.Text = total.ToString();
+
+        }
+
+
+    }
+}
